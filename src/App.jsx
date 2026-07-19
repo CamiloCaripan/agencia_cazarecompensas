ebad561730ada90f46c00e6bf3c814bf90135fcf
Camilo Caripan <camilo.caripan@inacapmail.cl>
2026-07-19 19:01:54 -0400

    commit inicial con base vite

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Logorythms</h1>
+      <p>bienvenidos al fin del mundo</p>
+    </>
+  )
+}
+
+
+export default App
